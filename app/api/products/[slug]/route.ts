bb95f8ae9aa352df008cefb64ebd891163ae4311
Alexandr Ryzhenkov <alex2505@bk.ru>
2024-08-11 19:15:43 +0300

    add route GET for product & products; upd schema.prisma; add tables to db;

--- app/api/products/[slug]/route.ts
+++ app/api/products/[slug]/route.ts
@@ -1,5 +1,5 @@
-import { prisma } from '../../../../prisma/prisma';
 import { NextResponse } from 'next/server';
+import { prisma } from '../../../../prisma/prisma';
 
 export async function GET(req, res) {
     const id = req.url.split("/").pop();

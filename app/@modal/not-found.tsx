f9a983da75709b765cc7f666cc3b8f6d91ca73ef
Alexandr Ryzhenkov <alex2505@bk.ru>
2024-08-14 14:44:58 +0300

    add NotFound;
    [
      NotFound
    ]

---
+++ app/@modal/not-found.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default async function NotFound() {
+
+  return (
+    <div className="not-found">
+        404 | This page could not be found.
+    </div>
+  )
+}
